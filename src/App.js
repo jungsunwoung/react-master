b53709b8beaf25d70c19a02158ea932fbd43e78e
jungsunwoung <51280827+jungsunwoung@users.noreply.github.com>
2022-03-07 18:37:29 +0900

    styled2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import styled from "styled-components";
 
-const Father = styled.div``;
+const Father = styled.div`
+  display: flex;
+`;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
 function App() {
   return (
     // 원래방법
-    <div style={{ display: "flex" }}>
-      <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
-      <div style={{ backgroundColor: "teal", width: 100, height: 100 }}></div>
-    </div>
+    <Father>
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </Father>
   );
 }
 
