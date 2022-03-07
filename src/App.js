5b2ab49dd32b5506fd8df9b8367b6cd8e196dce2
jungsunwoung <51280827+jungsunwoung@users.noreply.github.com>
2022-03-07 18:04:47 +0900

    styled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import styled from "styled-components";
+
+const Father = styled.div``;
 function App() {
-  return null;
+  return (
+    // 원래방법
+    <div style={{ display: "flex" }}>
+      <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
+      <div style={{ backgroundColor: "teal", width: 100, height: 100 }}></div>
+    </div>
+  );
 }
 
 export default App;
