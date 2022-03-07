ea589b49aefc36067e2e5c392b0029f1c80cb4fa
jungsunwoung <51280827+jungsunwoung@users.noreply.github.com>
2022-03-07 19:32:15 +0900

    styled2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
   display: flex;
 `;
+const rotateAnimation = keyframes`
+from {
+  transform:rotate(0deg);
+  border-radius:0px;
+}to{
+  transform:rotate(360deg);
+  border-radius:100px;
+}
+`;
 const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
+  background-color: tomato;
   width: 100px;
   height: 100px;
+  animation: ${rotateAnimation} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 48px;
+    }
+  }
 `;
 
 const Text = styled.span`
@@ -16,12 +35,24 @@ const Text = styled.span`
 const Circle = styled(Box)`
   border-radius: 50px;
 `;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
+`;
+
 function App() {
   return (
     // 원래방법
     <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Box>
+        <span>😄</span>
+      </Box>
     </Father>
   );
 }
