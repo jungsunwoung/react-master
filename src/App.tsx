d86d0bc39cc32a2041fe32a2d5fe0f6aa4587873
jungsunwoung <51280827+jungsunwoung@users.noreply.github.com>
2022-03-08 18:21:19 +0900

    typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styled, { keyframes } from "styled-components";
+import Circle from "./Circle";
+function App() {
+  return (
+    <div>
+      <Circle bgColor="teal"></Circle>
+      <Circle bgColor="tomato"></Circle>
+    </div>
+  );
+}
+
+export default App;
